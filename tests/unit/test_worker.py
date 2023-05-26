d3a4f1485b6839a6967d641aba8ede8aae0e65d3
Chip Goines <chip_goines@harvard.edu>
2023-05-25 23:54:37 -0400

    comment out the mocks to see if it increases coverage

--- tests/unit/test_worker.py
+++ tests/unit/test_worker.py
@@ -2,8 +2,8 @@ from etd.worker import Worker
 import requests
 
 
-class MockResponse:
-    text = "REST api is running."
+# class MockResponse:
+#     text = "REST api is running."
 
 
 class TestWorkerClass():
@@ -14,26 +14,26 @@ class TestWorkerClass():
         version = worker.get_version()
         assert version == expected_version
 
-    def test_api(self, monkeypatch):
+    # def test_api(self, monkeypatch):
 
-        def mock_get(*args, **kwargs):
-            return MockResponse()
+    #     def mock_get(*args, **kwargs):
+    #         return MockResponse()
 
-        # apply the monkeypatch for requests.get to mock_get
-        monkeypatch.setattr(requests, "get", mock_get)
-        expected_msg = "REST api is running."
-        worker = Worker()
-        msg = worker.call_api()
-        assert msg == expected_msg
+    #     # apply the monkeypatch for requests.get to mock_get
+    #     monkeypatch.setattr(requests, "get", mock_get)
+    #     expected_msg = "REST api is running."
+    #     worker = Worker()
+    #     msg = worker.call_api()
+    #     assert msg == expected_msg
 
-    def test_api_fail(self, monkeypatch):
+    # def test_api_fail(self, monkeypatch):
 
-        def mock_get(*args, **kwargs):
-            return MockResponse()
+    #     def mock_get(*args, **kwargs):
+    #         return MockResponse()
 
-        # apply the monkeypatch for requests.get to mock_get
-        monkeypatch.setattr(requests, "get", mock_get)
-        expected_msg = "REST api is NOT running."
-        worker = Worker()
-        msg = worker.call_api()
-        assert msg != expected_msg
+    #     # apply the monkeypatch for requests.get to mock_get
+    #     monkeypatch.setattr(requests, "get", mock_get)
+    #     expected_msg = "REST api is NOT running."
+    #     worker = Worker()
+    #     msg = worker.call_api()
+    #     assert msg != expected_msg
