0570203747b479749a29a45ad71b04602fd427a1
Chip Goines <chip_goines@harvard.edu>
2023-05-25 22:06:40 -0400

    temporarily bring the coverage down on the badge

--- tests/unit/test_worker.py
+++ tests/unit/test_worker.py
@@ -1,10 +1,20 @@
+import pytest
 from etd.worker import Worker
 
 
 class TestWorkerClass():
 
-    def test_version(self):
-        expected_version = "0.0.1"
-        worker = Worker()
-        version = worker.get_version()
-        assert version == expected_version
+    # def test_version(self):
+    #     expected_version = "0.0.1"
+    #     worker = Worker()
+    #     version = worker.get_version()
+    #     assert version == expected_version
+
+    # def test_call(self):
+    #     expected_msg = "REST api is running."
+    #     worker = Worker()
+    #     msg = worker.call_api()
+    #     assert msg == expected_msg
+
+    def test_nothing(self): #removing this, just want to bring the coverage down on the badge
+        assert True == True
