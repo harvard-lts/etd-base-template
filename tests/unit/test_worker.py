a8a7080a964e7d58f7a8afb1b1eee4c69d11a5a7
Chip Goines <chip_goines@harvard.edu>
2023-05-25 22:13:44 -0400

    restore coverage on the badge

--- tests/unit/test_worker.py
+++ tests/unit/test_worker.py
@@ -1,20 +1,10 @@
-# from etd.worker import Worker
+from etd.worker import Worker
 
 
 class TestWorkerClass():
 
-    # def test_version(self):
-    #     expected_version = "0.0.1"
-    #     worker = Worker()
-    #     version = worker.get_version()
-    #     assert version == expected_version
-
-    # def test_call(self):
-    #     expected_msg = "REST api is running."
-    #     worker = Worker()
-    #     msg = worker.call_api()
-    #     assert msg == expected_msg
-
-    # removing this, just want to bring the coverage down on the badge
-    def test_nothing(self):
-        assert True
+    def test_version(self):
+        expected_version = "0.0.1"
+        worker = Worker()
+        version = worker.get_version()
+        assert version == expected_version
