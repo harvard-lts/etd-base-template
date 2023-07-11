59afd4dc25dc14995724b7d0049ebee6586af4ce
chip goines <chip_goines@harvard.edu>
2023-07-10 21:26:55 -0400

    Etd 126 (#12)
    open telemetry

--- etd/worker.py
+++ etd/worker.py
@@ -1,5 +1,16 @@
 import os
 import requests
+from opentelemetry import trace
+# from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter
+
+provider = TracerProvider()
+processor = BatchSpanProcessor(ConsoleSpanExporter())
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__)
 
 """
 This is a template of a basic worker class for etds.
@@ -20,6 +31,11 @@ class Worker():
 
     # this is call to the DASH healthcheck for integration testing
     def call_api(self):
-        url = "https://dash.harvard.edu/rest/test"
-        r = requests.get(url)
-        return r.text
+        with tracer.start_as_current_span("server_request",
+                                          attributes={"endpoint":
+                                                      "/call_api"}):
+            url = "https://dash.harvard.edu/rest/test"
+            r = requests.get(url)
+            span = trace.get_current_span()
+            span.add_event("log", {"call.api": r.text})
+            return r.text
